489994e54d2a6fb95b1c27b52e1479e42636e8ce
YoGrT0104 <youp.grundel@gmail.com>
2023-12-19 10:01:01 +0100

    Auto stash before merge of "main" and "origin/main"
    Hopelijk mag dit in main

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -5,7 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+<<<<<<< HEAD
     public void StartGame()
+=======
+    public void PlayGame()
+>>>>>>> d8214157f194165629cbeb9cc40ac5fecdaf3f48
     {
         SceneManager.LoadSceneAsync(1);
     }
