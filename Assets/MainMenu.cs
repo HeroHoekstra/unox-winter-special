3b085a0d0de9f8623ab92454089912d7310aa7d1
YoGrT0104 <youp.grundel@gmail.com>
2023-12-19 10:07:48 +0100

    Merge conflict gefixt

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -5,11 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-<<<<<<< HEAD
-    public void StartGame()
-=======
     public void PlayGame()
->>>>>>> d8214157f194165629cbeb9cc40ac5fecdaf3f48
     {
         SceneManager.LoadSceneAsync(1);
     }
