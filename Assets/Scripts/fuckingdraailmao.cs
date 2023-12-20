7ddc4cd5c46a65ce86a197cf6de75997c365b90c
Hero Hoekstra <2172289@talnet.nl>
2023-12-20 13:16:09 +0100

    God has left
    :(

--- Assets/Scripts/fuckingdraailmao.cs
+++ Assets/Scripts/fuckingdraailmao.cs
@@ -6,6 +6,7 @@ public class fuckingdraailmao : MonoBehaviour
 {
     private Transform img;
     float currentAngle = 0;
+    float speed = 0;
     // Start is called before the first frame update
     void Start() {
         img = GetComponent<Transform>();
@@ -13,9 +14,9 @@ public class fuckingdraailmao : MonoBehaviour
 
     // Update is called once per frame
     void Update() {
-        currentAngle = currentAngle < 355 ? currentAngle + 1 : 0;
-        Debug.Log(currentAngle);
-
+        currentAngle = currentAngle < 358 ? currentAngle + speed : 0;
         img.rotation = Quaternion.Euler(0, 0, currentAngle);
+
+        speed += Time.deltaTime / 100;
     }
 }
